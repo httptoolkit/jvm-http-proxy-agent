7ce374a4e51af7aff0a29c4870ff1186fb12569e
Tim Perry <pimterry@gmail.com>
2021-03-03 13:26:07 +0100

    Make Apache v4 interception reliable enough to intercept Gradle
    Not really because Gradle is especially interesting, more that it's sufficiently
    common that it's likely to be a test case for many people, and it would be good
    if that worked! Previously it did basically work, AFAICT, but with loud errors
    during setup when HttpRoute couldn't be resolved. We now ensure that that's not
    required, so there's no errors: just perfectly intercepted HTTP(S).

--- src/main/java/tech/httptoolkit/javaagent/advice/apacheclient/ApacheV4ReturnProxyRouteAdvice.java
+++ src/main/java/tech/httptoolkit/javaagent/advice/apacheclient/ApacheV4ReturnProxyRouteAdvice.java
@@ -1,23 +1,30 @@
 package tech.httptoolkit.javaagent.advice.apacheclient;
 
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.routing.HttpRoute;
-import tech.httptoolkit.javaagent.HttpProxyAgent;
+
+import java.net.*;
 
 public class ApacheV4ReturnProxyRouteAdvice {
     @Advice.OnMethodExit
     public static void determineRoute(
-            @Advice.Return(readOnly = false) HttpRoute returnValue
+        // We type this dynamically, because in some cases (notably Gradle) we seemingly can't reach the
+        // HttpRoute type from ByteBuddy, only at runtime.
+        @Advice.Return(readOnly = false, typing = Assigner.Typing.DYNAMIC) Object returnValue
     ) {
+        HttpRoute existingValue = (HttpRoute) returnValue;
+        // We guarantee that the default proxy selector is always our own. This ensures that we can
+        // always grab the proxy URL without needing to access our injected classes.
+        Proxy proxy = ProxySelector.getDefault().select(URI.create("https://example.com")).get(0);
+        InetSocketAddress proxyAddress = (InetSocketAddress) proxy.address();
+
         returnValue = new HttpRoute(
-            returnValue.getTargetHost(),
-            returnValue.getLocalAddress(),
-            new HttpHost(
-                HttpProxyAgent.getAgentProxyHost(),
-                HttpProxyAgent.getAgentProxyPort()
-            ),
-            returnValue.isSecure()
+            existingValue.getTargetHost(),
+            existingValue.getLocalAddress(),
+            new HttpHost(proxyAddress.getHostString(), proxyAddress.getPort()),
+            existingValue.isSecure()
         );
     }
 }
