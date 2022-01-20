bd6f444d01e01d34ae23fe14e67a99e6866ad1cc
Tim Perry <pimterry@gmail.com>
2022-01-20 21:51:20 +0100

    Fix Netty interception when launched in Docker
    Within static fields, it was not possible to access project dependencies
    at runtime. Specifically, when launching the spring petclinic
    microservices demo, the API gateway that uses HttpClient crashed because
    reactor.netty.transport.ProxyProvider wasn't available when evaluating
    the static field.
    
    I'm fairly sure this is due to the app being launched with
    org.springframework.boot.loader.JarLauncher, which reconfigures the
    classpath during startup. To work around this, we defer loading these
    inaccessible types until the intercepted HttpClient constructor. That
    probably creates a very minor performance hit, but nothing significant.
    
    If this does become significant in future, we could probably preserve
    the fields, and make constructor initialization a one-off, but it's more
    complicated and risky, so let's not bother for now. Only matters if
    you're creating very very large numbers of HttpClients in a hot loop,
    which seems unlikely.

--- src/main/java/tech/httptoolkit/javaagent/advice/reactornetty/ReactorNettyResetAllConfigAdvice.java
+++ src/main/java/tech/httptoolkit/javaagent/advice/reactornetty/ReactorNettyResetAllConfigAdvice.java
@@ -13,30 +13,36 @@ import java.net.InetSocketAddress;
 
 public class ReactorNettyResetAllConfigAdvice {
 
-    public static final ProxyProvider agentProxyProvider = ProxyProvider.builder()
-        .type(ProxyProvider.Proxy.HTTP)
-        .address(new InetSocketAddress(
-            HttpProxyAgent.getAgentProxyHost(),
-            HttpProxyAgent.getAgentProxyPort()
-        ))
-        .build();
+    // Netty's HTTP Client works by creating a new client subclass, and passing the existing client's
+    // config. We hook that here: we rewrite the config whenever it's used, affecting all clients
+    // involved, and in practice anybody using config anywhere.
 
-    public static final SslProvider agentSslProvider;
-
-    public static final Field configSslField;
-    public static final Field proxyProviderField;
-
-    static {
-        try {
-            // Initialize our intercepted SSL provider:
-            agentSslProvider = SslProvider.builder()
-                .sslContext(
+    @Advice.OnMethodEnter
+    public static void beforeConstructor(
+        @Advice.Argument(value=0) HttpClientConfig baseHttpConfig
+    ) throws Exception {
+        // It would be nice to do this setup statically, but due to how some classloader configs work (e.g. Spring) it
+        // seems this can fail when run in a static block, so we just repeat the process for every interception:
+        final SslProvider agentSslProvider = SslProvider.builder()
+            .sslContext(
                     SslContextBuilder
-                    .forClient()
-                    .trustManager(HttpProxyAgent.getInterceptedTrustManagerFactory())
-                    .build()
-                ).build();
+                            .forClient()
+                            .trustManager(HttpProxyAgent.getInterceptedTrustManagerFactory())
+                            .build()
+            ).build();
 
+        final ProxyProvider agentProxyProvider = ProxyProvider.builder()
+            .type(ProxyProvider.Proxy.HTTP)
+            .address(new InetSocketAddress(
+                    HttpProxyAgent.getAgentProxyHost(),
+                    HttpProxyAgent.getAgentProxyPort()
+            ))
+            .build();
+
+        Field configSslField;
+        Field proxyProviderField;
+
+        try {
             // Rewrite the fields we want to mess with in the client config:
             configSslField = HttpClientConfig.class.getDeclaredField("sslProvider");
             configSslField.setAccessible(true);
@@ -46,16 +52,7 @@ public class ReactorNettyResetAllConfigAdvice {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
 
-    // Netty's HTTP Client works by creating a new client subclass, and passing the existing client's
-    // config. We hook that here: we rewrite the config whenever it's used, affecting all clients
-    // involved, and in practice anybody using config anywhere.
-
-    @Advice.OnMethodEnter
-    public static void beforeConstructor(
-        @Advice.Argument(value=0) HttpClientConfig baseHttpConfig
-    ) throws Exception {
         configSslField.set(baseHttpConfig, agentSslProvider);
         proxyProviderField.set(baseHttpConfig, agentProxyProvider);
     }
